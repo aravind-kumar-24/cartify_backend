4d77fcc2352896d9be7cdad5fe9b16408e3bac75
aravind-kumar-24 <aravindmpkas@gmail.com>
2026-05-06 22:58:51 +0530

    Buyer Registration along with Mail functionality & Database transactions has been completed

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/register')->group(function(){
     Route::post('/buyer_register', [RegistrationController::class, 'buyer_registration']);
+    Route::get('/buyer_email_verification/{buyer_id}', [RegistrationController::class, 'buyer_email_verification']);
 });
